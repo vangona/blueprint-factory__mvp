6869868b31fbc574dbd0727d907818af361413d4
vangona <rlarhksrud12@naver.com>
2022-03-06 00:10:41 +0900

    Add: Add Sentry

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 import App from 'components/App';
 import * as serviceWorker from 'serviceWorkerRegistration';
+
+Sentry.init({
+  dsn: "https://e8fe7365216b4314a1a1aa0c43785687@o1065943.ingest.sentry.io/6244136",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
