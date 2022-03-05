77a7fa15896aaf4632c9de2fb18234a2bfe4b72c
vangona <rlarhksrud12@naver.com>
2022-03-06 00:02:49 +0900

    Environment Setting

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import App from 'components/App';
+import * as serviceWorker from 'serviceWorkerRegistration';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
+
+if ('serviceWorker' in navigator) {
+  serviceWorker.register();
+}
